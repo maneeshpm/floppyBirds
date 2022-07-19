67e01cb3e7999dc18caf87f971b017ceebff44b6
Maneesh P M <manu.pm55@gmail.com>
2022-07-19 11:46:39 +0530

    Add game physics

--- bird.go
+++ bird.go
@@ -9,11 +9,15 @@ import (
 
 const (
 	framePath = "res/imgs/frame-%d.png"
+	gravity   = 0.048
 )
 
 type bird struct {
 	iter     int
 	textures []*sdl.Texture
+
+	y, yspeed float64
+	x, xspeed float64
 }
 
 func newBird(ren *sdl.Renderer) (*bird, error) {
@@ -26,14 +30,23 @@ func newBird(ren *sdl.Renderer) (*bird, error) {
 		textures = append(textures, texture)
 	}
 
-	return &bird{textures: textures}, nil
+	return &bird{textures: textures, x: 10, y: winHeight / 2, xspeed: 1}, nil
 }
 
 func (bird *bird) paint(ren *sdl.Renderer) error {
-	rect := &sdl.Rect{W: 100, H: 86, X: 10, Y: winHeight/2 - 43/2}
+	rect := &sdl.Rect{W: 200, H: 150, X: int32(bird.x), Y: (winHeight - int32(bird.y)) - 43/2}
 
 	bird.iter++
-	frameSelector := bird.iter / 10 % len(bird.textures)
+
+	bird.yspeed += gravity
+	bird.y -= bird.yspeed
+	if bird.y < 0 {
+		bird.yspeed *= -1
+		bird.y = 0
+	}
+
+	bird.x += bird.xspeed
+	frameSelector := bird.iter / 15 % len(bird.textures)
 
 	if err := ren.Copy(bird.textures[frameSelector], nil, rect); err != nil {
 		return fmt.Errorf("Could not copy texture: %v", err)
