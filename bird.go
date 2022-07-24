e794684663e64d5e737a7004865edd767b2f1d41
Maneesh P M <manu.pm55@gmail.com>
2022-07-24 12:53:37 +0530

    Add collision physics

--- bird.go
+++ bird.go
@@ -10,8 +10,11 @@ import (
 
 const (
 	framePath = "res/imgs/frame-%d.png"
-	gravity   = 0.2
+	gravity   = 0.3
 	jumpSpeed = 8
+
+	birdWidth  = 120
+	birdHeight = 120
 )
 
 type bird struct {
@@ -21,8 +24,10 @@ type bird struct {
 	iter     int
 	textures []*sdl.Texture
 
-	y, yspeed float64
-	x, xspeed float64
+	W, H int32
+
+	x, y   int32
+	yspeed float64
 }
 
 func newBird(ren *sdl.Renderer) (*bird, error) {
@@ -35,7 +40,7 @@ func newBird(ren *sdl.Renderer) (*bird, error) {
 		textures = append(textures, texture)
 	}
 
-	return &bird{textures: textures, x: 10, y: winHeight / 2, xspeed: 1}, nil
+	return &bird{textures: textures, x: 50, y: winHeight / 2, W: birdWidth, H: birdHeight}, nil
 }
 
 func (bird *bird) isDead() bool {
@@ -60,19 +65,17 @@ func (bird *bird) update() {
 	bird.iter++
 
 	bird.yspeed += gravity
-	bird.y -= bird.yspeed
+	bird.y -= int32(bird.yspeed)
 	if bird.y < 0 {
 		bird.dead = true
 	}
-
-	bird.x += bird.xspeed
 }
 
 func (bird *bird) paint(ren *sdl.Renderer) error {
 	bird.mu.RLock()
 	defer bird.mu.RUnlock()
 
-	rect := &sdl.Rect{W: 200, H: 150, X: int32(bird.x), Y: (winHeight - int32(bird.y)) - 43/2}
+	rect := &sdl.Rect{W: bird.W, H: bird.H, X: int32(bird.x), Y: (winHeight - int32(bird.y)) - 43/2}
 	frameSelector := bird.iter / 10 % len(bird.textures)
 
 	if err := ren.Copy(bird.textures[frameSelector], nil, rect); err != nil {
@@ -87,6 +90,31 @@ func (bird *bird) jump() {
 	defer bird.mu.Unlock()
 
 	bird.yspeed = -jumpSpeed
+}
+
+func (bird *bird) check(pipe *pipe) {
+	bird.mu.Lock()
+	defer bird.mu.Unlock()
+	pipe.mu.RLock()
+	defer pipe.mu.RUnlock()
+
+	if bird.x+bird.W < pipe.x {
+		return
+	}
+
+	if pipe.x+pipe.w < bird.x {
+		return
+	}
+
+	if pipe.inverted && winHeight-pipe.h > bird.y+bird.H/2 {
+		return
+	}
+
+	if !pipe.inverted && bird.y-bird.H/2 > pipe.h {
+		return
+	}
+
+	bird.dead = true
 }
 
 func (bird *bird) destroy() {
